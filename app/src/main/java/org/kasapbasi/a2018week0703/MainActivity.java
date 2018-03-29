7c8d08172cc63b024ee2f0d0d366eafa662c0bb2
mckasap <mckasap@gmail.com>
2018-03-29 12:33:45 +0300

    NO Action BAR FULL Screen

--- app/src/main/java/org/kasapbasi/a2018week0703/MainActivity.java
+++ app/src/main/java/org/kasapbasi/a2018week0703/MainActivity.java
@@ -1,5 +1,6 @@
 package org.kasapbasi.a2018week0703;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,11 +9,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.graphics.Color.*;
+import android.widget.Toast;
+
+import static android.graphics.Color.GREEN;
 
 public class MainActivity extends AppCompatActivity  implements SensorEventListener{
  SensorManager sm;
-
-
+ TextView tv;
+    private boolean color = false;
+long lastUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 // en üstteki Minnak ikonlar (saat, GSM operator, data vb) kaldırma kodu
@@ -23,9 +30,84 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    tv= (TextView) findViewById(R.id.textView);
+    tv.setBackgroundColor(GREEN);
+
+    sm= (SensorManager) getSystemService(SENSOR_SERVICE);
+    lastUpdate=System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
+        }
+    }
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z)
+                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = event.timestamp;
+
+        if (accelationSquareRoot >= 2) //
+        {
+            if (actualTime - lastUpdate < 200) {
+                return;
+            }
+            lastUpdate = actualTime;
+            Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT)
+                    .show();
+            if (color) {
+                tv.setBackgroundColor(Color.GREEN);
+            } else {
+                tv.setBackgroundColor(Color.RED);
+            }
+            color = !color;
+        }
+    }
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        sm.registerListener(this,
+                sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        super.onResume();
 
 
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
+class mylistener implements SensorEventListener{
+
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
